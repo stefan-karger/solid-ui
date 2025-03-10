54b920c0f61c7da176a6ab728d4049dc6d12cb1c
Stefan E-K <stefan.eideloth@gmail.com>
2025-03-10 14:18:05 +0100

    fix: delta-bar indicator know matches bar color

---
+++ .changeset/clean-rice-crash.md
@@ -0,0 +1,5 @@
+---
+"docs": patch
+---
+
+fix: delta-bar indicator know matches bar color
--- apps/docs/src/registry/ui/delta-bar.tsx
+++ apps/docs/src/registry/ui/delta-bar.tsx
@@ -16,6 +16,11 @@ const DeltaBar: Component<DeltaBarProps> = (rawProps) => {
   )
   const [local, others] = splitProps(props, ["value", "isIncreasePositive", "class"])
 
+  const barColor = () =>
+    (local.value > 0 && local.isIncreasePositive) || (local.value < 0 && !local.isIncreasePositive)
+      ? "bg-success-foreground"
+      : "bg-error-foreground"
+
   return (
     <div
       class={cn("relative flex h-2 w-full items-center rounded-full bg-secondary", local.class)}
@@ -24,28 +29,16 @@ const DeltaBar: Component<DeltaBarProps> = (rawProps) => {
       <div class="flex h-full w-1/2 justify-end">
         <Show when={local.value < 0}>
           <div
-            class={cn(
-              "rounded-l-full",
-              (local.value > 0 && local.isIncreasePositive) ||
-                (local.value < 0 && !local.isIncreasePositive)
-                ? "bg-success-foreground"
-                : "bg-error-foreground"
-            )}
+            class={cn("rounded-l-full", barColor())}
             style={{ width: `${Math.abs(local.value)}%` }}
           />
         </Show>
       </div>
-      <div class={cn("z-10 h-4 w-1 rounded-full bg-primary ring-2 ring-background")} />
+      <div class={cn("z-10 h-4 w-1 rounded-full ring-2 ring-background", barColor())} />
       <div class="flex h-full w-1/2 justify-start">
         <Show when={local.value > 0}>
           <div
-            class={cn(
-              "rounded-r-full",
-              (local.value > 0 && local.isIncreasePositive) ||
-                (local.value < 0 && !local.isIncreasePositive)
-                ? "bg-success-foreground"
-                : "bg-error-foreground"
-            )}
+            class={cn("rounded-r-full", barColor())}
             style={{ width: `${Math.abs(local.value)}%` }}
           />
         </Show>
