stefan-karger/solid-ui

commit ebf3265c861fa4fc36f35489600e725763163976

feat: setup project with solidui-cli using deno (#181)
* feat: setup project with solidui-cli using deno

Using `deno run @solidui-cli@latest init`.

Signed-off-by: João Augusto Costa Branco Marado Torres <torres.dev@disroot.org>

* feat: adding components with solidui-cli using deno

Using `deno run npm:solidui-cli@latest add button` for example.

Signed-off-by: João Augusto Costa Branco Marado Torres <torres.dev@disroot.org>

* refactor: using deno v2.3 registry flags

Signed-off-by: João Augusto Costa Branco Marado Torres <torres.dev@disroot.org>

* chore: add changeset

Signed-off-by: João Augusto Costa Branco Marado Torres <torres.dev@disroot.org>

---------

Signed-off-by: João Augusto Costa Branco Marado Torres <torres.dev@disroot.org>

packages/cli/src/commands/add.ts
@@ -148,7 +148,11 @@ export const add = new Command()
         // install dependencies
         if (item.dependencies?.length) {
           const packageManager = await getPackageManager(cwd)
-          await execa(packageManager, ["add", ...item.dependencies], { cwd })
+          await execa(packageManager, [
+            "add",
+            packageManager === "deno" ? "--npm" : "",
+            ...item.dependencies,
+          ], { cwd });
         }
       }
 

packages/cli/src/commands/init.ts
@@ -97,7 +97,11 @@ export const init = new Command()
       spinner.start(`Installing dependencies...`)
 
       const packageManager = await getPackageManager(cwd)
-      await execa(packageManager, ["add", ...PROJECT_DEPENDENCIES], { cwd })
+      await execa(packageManager, [
+        "add",
+        packageManager === "deno" ? "--npm" : "",
+        ...PROJECT_DEPENDENCIES,
+      ], { cwd });
 
       spinner.stop(`Dependencies installed.`)
 

packages/cli/src/utils/get-package-manager.ts
@@ -2,7 +2,7 @@ import { detect } from "@antfu/ni"
 
 export async function getPackageManager(
   targetDir: string
-): Promise<"yarn" | "pnpm" | "bun" | "npm"> {
+): Promise<"yarn" | "pnpm" | "bun" | "npm" | "deno"> {
   const packageManager = await detect({ programmatic: true, cwd: targetDir })
 
   if (packageManager === "yarn@berry") return "yarn"

commit 7c534d58e462ec41250b70e68394ff3582c0ea7c

fix(packages/cli): package manager bun error
Error caused by #181.

Run bunx solidui-cli@latest add button, error:

ExecaError: Command failed with exit code 1: bun add '' '@kobalte/core'

packages/cli/src/commands/add.ts
@@ -152,7 +152,7 @@ export const add = new Command()
             "add",
             packageManager === "deno" ? "--npm" : "",
             ...item.dependencies,
-          ], { cwd });
+          ].filter(Boolean), { cwd });
         }
       }
 