4710c6640639c8ae0e3f28caebf841d2f87fbec2
Jonir Rings <peterpuyi@live.cn>
2026-08-05 10:52:55 +0800

    Fix conditional argument for package manager command
    empty string will cause
    ```
    [ERR_PNPM_SPEC_NOT_SUPPORTED_BY_ANY_RESOLVER] "latest" isn't supported by any available resolver.
    ```
    undefined works good under UOS, test required for other system/shell

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -150,7 +150,7 @@ export const add = new Command()
           const packageManager = await getPackageManager(cwd)
           await execa(packageManager, [
             "add",
-            packageManager === "deno" ? "--npm" : "",
+            packageManager === "deno" ? "--npm" : undefined,
             ...item.dependencies,
           ], { cwd });
         }
