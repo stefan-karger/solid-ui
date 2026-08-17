4a6a0e170606a2152d6c425e5f0387013c3fcbfc
Joey Grable <hello@joeygrable.com>
2026-08-17 16:40:47 -0700

    added docs page for calendar date-picker component examples

--- apps/docs/src/config/docs.ts
+++ apps/docs/src/config/docs.ts
@@ -93,12 +93,16 @@ export const docsConfig: Config = {
           href: "/docs/components/collapsible"
         },
         {
-          title: "Dialog",
-          href: "/docs/components/dialog"
-        },
-        {
           title: "Context Menu",
           href: "/docs/components/context-menu"
+        },
+        {
+          title: "Date Picker",
+          href: "/docs/components/date-picker"
+        },
+        {
+          title: "Dialog",
+          href: "/docs/components/dialog"
         },
         {
           title: "Dropdown Menu",
